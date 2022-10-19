be4e5db528e3676c09aeaaa6cb65f854be0fe4df
Mahmudur Rashid <70084964+Mahmudur-Rashid@users.noreply.github.com>
2022-10-19 22:55:52 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,107 @@
+const game = ()=> {
+    let pScore = 0;
+    let cScore = 0;
+
+    //start game
+    const startGame = ()=> {
+        const playBtn = document.querySelector('.intro button');
+        const introScreen = document.querySelector('.intro');
+        const match = document.querySelector('.match');
+
+        playBtn.addEventListener('click', ()=>{
+
+          introScreen.classList.add('fadeOut');
+          match.classList.add('fadeIn');
+
+        });
+      }
+      //start match
+    const playMatch = () => {
+      const options = document.querySelectorAll('.options button');
+      const playerHand = document.querySelector('.player-hand');
+      const computerHand = document.querySelector('.computer-hand');
+      const computerOptions = ["rock", "paper", "scissors"];
+      const hands = document.querySelectorAll(".hands img");     
+      
+      hands.forEach(hand =>{
+        hand.addEventListener("animationend", function(){
+          this.style.animation = "";
+        });
+      });
+      options.forEach((option) => {
+        // computer hand
+        option.addEventListener("click", function() {
+          console.log(this);
+          const computerNumber = Math.floor(Math.random() * 3);
+          const computerChoice = computerOptions[computerNumber]
+
+          setTimeout(() => {
+            compareHands(this.textContent, computerChoice);
+            playerHand.src = `./photos/${this.textContent}.png`;
+            computerHand.src = `./photos/${computerChoice}.png`;  
+          }, 2000)
+          playerHand.style.animation = "shakePlayer 2s ease";
+          computerHand.style.animation = "shakeComputer 2s ease";
+        });
+      });
+    };
+
+    const updateScore = () => {
+      playerScore = document.querySelector(".player-score p");
+      computerScore = document.querySelector(".computer-score p");
+      playerScore.textContent = pScore;
+      computerScore.textContent = cScore;
+    } 
+    //compare hands to see who wins
+    const compareHands = (playerChoice, computerChoice) =>{
+      const winner = document.querySelector(".winner")
+      if (playerChoice === computerChoice) {
+        winner.textContent = "It is a tie";
+        return;
+      }
+      if (playerChoice === "rock") {
+        if (computerChoice === "scissors"){
+          winner.textContent = "Player wins";
+          pScore++;
+          updateScore()
+          return;
+        } else{
+          winner.textContent = "Computer Wins";
+          cScore++;
+          updateScore()
+          return;
+        }
+      }
+      if (playerChoice === "paper") {
+        if (computerChoice === "rock"){
+          winner.textContent = "Player wins";
+          pScore++;
+          updateScore()
+          return;
+        } else{
+          winner.textContent = "Computer Wins";
+          cScore++;
+          updateScore()
+          return;
+        }
+      }
+      if (playerChoice === "scissors") {
+        if (computerChoice === "paper"){
+          winner.textContent = "Player wins";
+          pScore++;
+          updateScore()
+          return;
+        } else{
+          winner.textContent = "Computer Wins";
+          cScore++;
+          updateScore()
+          return;
+        }
+      }
+    }
+
+  startGame();
+  playMatch();
+}
+
+game();
